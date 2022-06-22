332dd1f691fb6725d35fb3c2f701816d6373cf1d
Lucas <90116481+LucasRibeiro19@users.noreply.github.com>
2022-06-22 22:56:37 +0200

    Homepage title animation

--- src/animations.js
+++ src/animations.js
@@ -1,1 +1,13 @@
-import {gsap} from "gsap";
+const title = document.querySelector("#job");
+
+
+new Typewriter(title,{
+    deleteSpeed: 20
+})
+.pauseFor(1000)
+.typeString('<span style="color:#2F2CBA;font-weight:bold">Front-End </span>')
+.typeString('<span style="color:#FFFF">Developper</span>')
+.start()
+
+
+
