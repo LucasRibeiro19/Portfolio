a68a7cd67d42cd89b5d5cb5d8284e2e63f245a01
Lucas <90116481+LucasRibeiro19@users.noreply.github.com>
2022-06-23 22:27:06 +0200

    Added medias links and scroll down button

--- src/animations.js
+++ src/animations.js
@@ -1,4 +1,7 @@
 const title = document.querySelector("#job");
+const github = document.querySelector(".fa-brands.fa-github.fa-3x");
+const linkedIn = document.querySelector(".fa-brands.fa-linkedin-in.fa-3x");
+const accroche = document.querySelector("#accroche");
 
 
 new Typewriter(title,{
@@ -6,8 +9,25 @@ new Typewriter(title,{
 })
 .pauseFor(1000)
 .typeString('<span style="color:#2F2CBA;font-weight:bold">Front-End </span>')
-.typeString('<span style="color:#FFFF">Developper</span>')
+.typeString('<span style="color:#FFFF">Developer</span>')
 .start()
 
+gsap.from(github,{
+    x:-20,
+    opacity:0,
+    duration: 1,
+    delay:5,
+});
 
+gsap.from(linkedIn,{
+    x:20,
+    opacity:0,
+    duration: 1,
+    delay:5,
+});
+gsap.from(accroche,{
+    opacity:0,
+    duration: 1,
+    delay:5.2,
+});
 
